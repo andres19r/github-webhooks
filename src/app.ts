a92e6760c69d318a89c944da82073f45789d2d0c
Andres Rivero <andres19rivero@gmail.com>
2024-01-11 16:48:21 -0400

    feat: create express app with initial endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hello world");
+import express from "express";
+import { envs } from "./config";
+
+(() => {
+  main();
+})();
+
+function main() {
+  const app = express();
+
+  app.post("/api/github", (req, res) => {
+    res.json("Github Endpoint");
+  });
+
+  app.listen(envs.PORT, () => {
+    console.log(`App running on port ${envs.PORT}`);
+  });
+}
