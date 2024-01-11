e2ba3c4db83f1b178fb16ff853b56965c8eb7626
Andres Rivero <andres19rivero@gmail.com>
2024-01-11 16:56:41 -0400

    feat!: create github controller to use in endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { envs } from "./config";
+import { GithubController } from "./presentation/github/controller";
 
 (() => {
   main();
@@ -8,9 +9,9 @@ import { envs } from "./config";
 function main() {
   const app = express();
 
-  app.post("/api/github", (req, res) => {
-    res.json("Github Endpoint");
-  });
+  const controller = new GithubController();
+
+  app.post("/api/github", controller.webhookHandler);
 
   app.listen(envs.PORT, () => {
     console.log(`App running on port ${envs.PORT}`);
