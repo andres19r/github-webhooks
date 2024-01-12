6d11896cae5c872ea0b05ecdc3514c405a6b6281
Andres Rivero <andres19rivero@gmail.com>
2024-01-12 11:06:56 -0400

    feat: set up env var for discord url

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,4 +3,6 @@ import { get } from "env-var";
 
 export const envs = {
   PORT: get("PORT").required().asPortNumber(),
+
+  DISCORD_WEBHOOK_URL: get("DISCORD_WEBHOOK_URL").required().asString(),
 };
