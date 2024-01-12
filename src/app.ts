cd18388845b4509b2fdfadc8e54fb9c0512d3133
Andres Rivero <andres19rivero@gmail.com>
2024-01-12 12:15:55 -0400

    feat: implement & use middleware to verify github token

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { envs } from "./config";
 import { GithubController } from "./presentation/github/controller";
+import { GitHubSha256Middleware } from "./presentation/middlewares/github-sha256.middleware";
 
 (() => {
   main();
@@ -12,6 +13,8 @@ function main() {
   const controller = new GithubController();
 
   app.use(express.json());
+
+  app.use(GitHubSha256Middleware.verifySignature);
 
   app.post("/api/github", controller.webhookHandler);
 
